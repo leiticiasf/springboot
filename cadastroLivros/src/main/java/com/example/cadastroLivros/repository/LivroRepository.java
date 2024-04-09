c5498949b1a764a7676fa29e1a9ad6b24a8e31d5
Letícia <111643803+leiticiasf@users.noreply.github.com>
2024-04-09 10:52:35 -0300

    BACKEND

---
+++ cadastroLivros/src/main/java/com/example/cadastroLivros/repository/LivroRepository.java
@@ -0,0 +1,9 @@
+
+package com.example.cadastroLivros.repository;
+
+import com.example.cadastroLivros.model.Livro;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface LivroRepository extends JpaRepository<Livro, Long> {
+}
